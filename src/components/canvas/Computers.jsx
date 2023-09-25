2c60cfb7b137f42134b5114f3b3b5b1a4eb43f97
NNahkan <hoksuz2424@gmail.com>
2023-09-24 23:10:40 -0400

    Problem fixed

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -11,6 +11,14 @@ const Computers = () => {
     <mesh>
       <hemisphereLight intensity={0.15} groundColor="black" />
       <pointLight intensity={1} />
+      <spotLight
+        position={[-20, 50, 10]}
+        angle={0.12}
+        penumbra={1}
+        intensity={1}
+        castShadow
+        shadow-mapSize={1024}
+      />
       <primitive
         object={computer.scene}
         scale={0.75}
@@ -20,7 +28,7 @@ const Computers = () => {
     </mesh>
   );
 };
-
+// https://stackoverflow.com/questions/75655381/the-react-three-fiber-mesh-tag-is-unrecognized-in-this-browser
 const ComputerCanvas = () => {
   return (
     <Canvas
@@ -29,7 +37,7 @@ const ComputerCanvas = () => {
       camera={{ position: [20, 3, 5], fov: 25 }}
       gl={{ preserveDrawingBuffer: true }}
     >
-      <Suspense fallback={<CanvasLoader />}>
+      <Suspense>
         <OrbitControls
           enableZoom={false}
           maxPolarAngle={Math.PI / 2}
@@ -42,4 +50,4 @@ const ComputerCanvas = () => {
   );
 };
 
-export default Computers;
+export default ComputerCanvas;
